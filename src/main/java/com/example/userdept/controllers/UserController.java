519f5b6547c88556a5edf57dd886d8e3a7776e3b
Erick Lemos <ericklemos.dev@gmail.com>
2024-08-15 16:40:11 -0300

    GET ALL Method

--- src/main/java/com/example/userdept/controllers/UserController.java
+++ src/main/java/com/example/userdept/controllers/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
@@ -23,5 +25,13 @@ public class UserController {
         BeanUtils.copyProperties(userRecordDto, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserModel>> getAll(){
+        List<UserModel> usersList = userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(usersList);
+    }
+
+
 
 }
